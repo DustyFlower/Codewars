5e546ffe336b017b97485ea4637494d9b9513f7f
DustyFlower <110167132+DustyFlower@users.noreply.github.com>
2023-09-21 20:20:44 +0300

    Update priceOfMangoes.js
    Fixed task

--- priceOfMangoes.js
+++ priceOfMangoes.js
@@ -1,8 +1,8 @@
 function mango(quantity, price)
 {
-  const paidMangoes = (quantity / 3) * 2;
+  const paidMangoes = Math.floor(quantity / 3) * 2 + (quantity % 3);
 
   const totalCost = paidMangoes * price;
 
-  return totalCost;
+  return totalCost
 }
