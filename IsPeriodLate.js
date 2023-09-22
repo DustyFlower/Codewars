3434576621ee6c3cf2bafabb97f1da31cba84ea5
DustyFlower <110167132+DustyFlower@users.noreply.github.com>
2023-09-22 15:07:42 +0300

    Add files via upload

---
+++ IsPeriodLate.js
@@ -0,0 +1,11 @@
+function periodIsLate(last, today, cycleLength)
+{
+    if (((today - last) / (1000 * 60 * 60 * 24)) > cycleLength)
+    {
+        return true;
+    } else
+    {
+  
+        return false;
+    }
+}
